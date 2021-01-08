ed454132e85a1800d9af99246f93610f90f1df8b
Stanko Bebek <stanko.bebek82@gmail.com>
2021-01-08 23:36:34 +0100

    created frontend for entrance quiz

---
+++ resources/js/components/QuestionBox.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+
+	<v-container>
+	   <v-stepper v-model="e1">
+	    <v-stepper-header>
+	        <template v-for="n in steps">
+	          <v-stepper-step
+	            :key="`${n}-step`"
+	            :complete="e1 > n"
+	            :step="n"
+	            editable
+	          >
+	            
+	          </v-stepper-step>
+
+	          <v-divider
+	            v-if="n !== steps"
+	            :key="n"
+	          ></v-divider>
+	        </template>
+      	</v-stepper-header>
+
+	    <v-stepper-items>
+	      <v-stepper-content step='1'>
+	        <v-card
+	          class="mb-12"
+	          color="grey lighten-1"
+	          height="200px"
+	        >
+	        	<v-row style="height: 100%;">
+	        		<v-col cols="12" class="d-flex justify-center align-center" style="margin: 15px;">
+	        			<p class="text-h6">Ovo je neko pitanje iz baze podata CodeSUM?</p>
+	        		</v-col>
+	        	</v-row>
+
+	        </v-card>
+
+	        <v-sheet>
+	        	<v-container class="pa-15">
+		        	<div class="answer__button">Odgovor 1</div>
+		        	<div class="answer__button">Odgovor 2</div>
+		        	<div class="answer__button">Odgovor 3</div>
+		        	<div class="answer__button">Odgovor 4</div>
+	        	</v-container>
+	        </v-sheet>
+
+	        <div class="d-flex justify-end">
+		        <v-btn
+		          color="primary"
+		          @click="e1 = 2"
+		          class="mr-3"
+		        >
+		          Continue
+		        </v-btn>
+
+		        <v-btn color="red">
+		          Cancel
+		        </v-btn>
+	        </div>
+	      </v-stepper-content>
+	    </v-stepper-items>
+	  </v-stepper>
+	</v-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+	data () {
+      return {
+        e1: 1,
+        steps: 5
+      }
+    },
+}
+</script>
+
+<style>
+	.answer__button{
+		background: lightgrey;
+		padding: 20px;
+		border-radius: 10px;
+		text-align:center;
+		margin-top: 15px;
+		font-size: 20px;
+	}
+</style>
--- resources/js/views/EntranceQuiz.vue
+++ resources/js/views/EntranceQuiz.vue
@@ -3,15 +3,19 @@
       <Navbar />
 
       <h1>Ovo je ulazni kviz</h1>
+
+      <QuestionBox />
       
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue';
+import QuestionBox from '../components/QuestionBox.vue';
 export default {
     components: {
-        Navbar
+        Navbar,
+        QuestionBox
     }
 }
 </script>
