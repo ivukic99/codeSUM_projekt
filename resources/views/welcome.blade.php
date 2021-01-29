f1708799895d8d0bcde3f680f05658f5529ba6ff
Igor Vukic <64687902+ivukic99@users.noreply.github.com>
2021-01-29 12:53:27 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,6 @@
             <App />
         </div>
 
-        <script src="{{ asset('js/app.js?version=2') }}"></script>
+        <script src="{{ asset('js/app.js?version=3') }}"></script>
     </body>
 </html>
