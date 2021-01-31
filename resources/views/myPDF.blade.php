3af1fcc9ce852c210991d21188bc325057a055a1
Igor Vukic <igor.vukic8877@gmail.com>
2021-01-31 23:05:40 +0100

    certifikat pdf

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -1,54 +1,64 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>codeSUM certifikat</title>
+    <style>
+        .main-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        .child-container{
+            font-family: cursive;
+            border-radius: 15px;
+            height: 480px;
+            width: 700px;
+            background: #e6ffff;
+            line-height: 2;
+            border-style: dashed;
+        }
+        .lesson-text {
+            text-align: center;
+        }
 
-    <title>CodeSUM certifikat</title>
+        .span1 {
+            float: left;
+            padding-left: 30px
+        }
+
+        .span2 {
+            float: right;
+            padding-right: 30px;
+        }
+    </style>
 </head>
-
 <body>
-    <h1>{{ $user->name }}</h1>
-    <div class="container">
 
-
-        <div class="card mt-4" style="background-color: #e7f3fd;">
-            <div class="text-center mt-4">
-                <h1 class="font-weight-bolder">C O D E <span style="color: darkorange"> S U M</span></h1>
-                <h2>Certifikat programiranja</h2>
-                <h3 class="mt-4">Č E S T I T A M O!</h3>
-                <h4>Igor Vukic</h4>
+    <div class="main-container">
+        <div class="child-container">
+            <div class="lesson-text">
+                <h1>C O D E <span style="color: darkorange"> S U M</span></h1>
+                <h3>Certifikat programiranja</h3>
+                <h3 >C E S T I T A M O!</h3>
+                <h4>Uspijesno ste polozili tecaj</h4>
+                <h2>{{ $user->name }}  {{ $user->surname }}</h2>
             </div>
-
-            <div class="card-body text-center">
-                <h5 class="card-title">Uspješno ste riješili sve telajeve</h5><br><br>
-                <div class="row">
-                    <div class="col-4">
-                        <p>____________________</p>
-                        <p>Potpis člana codeSUM</p>
-                    </div>
-                    <div class="col-4">
-                        <p>Datum: 12.21.32132</p>
-                    </div>
-                    <div class="col-4">
-                        <p>____________________</p>
-                        <p>Potpis ovlaštenog lica</p>
-                    </div>
+            <div class="lesson-link">
+                <div>
+                    <span class="span1">
+                        <p>______________</p>
+                        <p>Clan codeSUM</p>
+                    </span>
+                    <span class="span2">
+                        <p>______________</p>
+                        <p>Ovlasteno lice</p>
+                    </span>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 </body>
-
 </html>
