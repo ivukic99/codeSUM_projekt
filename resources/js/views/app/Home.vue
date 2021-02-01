2b0db3aff74167f4b3768e2b4846785fc0f4ac08
Igor Vukic <igor.vukic8877@gmail.com>
2021-02-01 20:58:53 +0100

    finish

--- resources/js/views/app/Home.vue
+++ resources/js/views/app/Home.vue
@@ -1,32 +1,38 @@
 <template>
-    <div>
-        <NavbarUser />
-        
-        <v-container>
-            <!--{{ getUserDetails.details }}-->
-            <h5>Ovaj dio je još uvijek u izradi!</h5>
-        </v-container>
-    </div>
+  <div>
+    <NavbarUser />
+
+    <v-container>
+      <!--{{ getUserDetails.details }}-->
+      <div class="child-container">
+        <v-img
+          src="https://i.ibb.co/3FhJxVq/undraw-work-in-progress-uhmv.png"
+        ></v-img>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import NavbarUser from '../../components/app/NavbarUser.vue';
+import { mapGetters } from "vuex";
+import NavbarUser from "../../components/app/NavbarUser.vue";
 export default {
-    components:{
-        NavbarUser
-    },
-    computed: {
-        ...mapGetters([
-            'getUserDetails'
-        ])
-    },
-    created(){
-        this.$store.dispatch('setUserDetails')
-    }
-}
+  components: {
+    NavbarUser,
+  },
+  computed: {
+    ...mapGetters(["getUserDetails"]),
+  },
+  created() {
+    this.$store.dispatch("setUserDetails");
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.child-container {
+  text-align: justify;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,6 @@
             <App />
         </div>
 
-        <script src="{{ asset('js/app.js?version=14') }}"></script>
+        <script src="{{ asset('js/app.js?version=15') }}"></script>
     </body>
 </html>
