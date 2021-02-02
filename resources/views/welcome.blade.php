9c156f3dbd173ab238668385b2084b06577e4a77
Igor Vukic <igor.vukic8877@gmail.com>
2021-02-02 19:32:31 +0100

    ende

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,6 @@
             <App />
         </div>
 
-        <script src="{{ asset('js/app.js?version=16') }}"></script>
+        <script src="{{ asset('js/app.js?version=17') }}"></script>
     </body>
 </html>
